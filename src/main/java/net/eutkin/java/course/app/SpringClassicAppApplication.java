fb2db0abd0c25b1e1156c3a7c85e1003f3943650
eutkin <000Vincit00{>
2019-01-16 11:33:49 +0300

    Initial commit

---
+++ src/main/java/net/eutkin/java/course/app/SpringClassicAppApplication.java
@@ -0,0 +1,14 @@
+package net.eutkin.java.course.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringClassicAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringClassicAppApplication.class, args);
+    }
+
+}
+
